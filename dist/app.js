04c6c55ecbd21145a07caade9d567344cd5c9d8e
realbear <realbearinc@gmail.com>
2020-01-05 15:56:04 -0800

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,6 @@
 var tl = gsap.timeline();
+
+
 var sound = new Howl({
     src: ['Party.mp3'],
     volume: 0.5
@@ -16,14 +18,14 @@ client.connect();
 
 client.on('message', (channel, tags, message, self) => {
     if (message == 'bingSmile' && tags['display-name'] == 'DrFate') {
+        tl.restart();
         tl.fromTo(".smile", { y: -200 }, { duration: 2, delay: 1, ease: "bounce.out", y: 1180 });
         tl.to(".smile", { duration: 3, ease: " circ.in", x: 200 });
+    } else if (message == 'bingC' && tags['display-name'] == 'DrFate') {
         tl.restart();
-    }
-    else if (message == '!bus' && tags['display-name'] == 'DrFate') {
         sound.play();
-        tl.to(".bus", { duration: 15, repeat: 13, ease: " circ.in", x: -3000 });
-        tl.restart();
+        tl.to(".bus", { duration: 15, repeat: 5, ease: " circ.in", x: -3000 });
+        sound.stop();
     }
 
     console.log(`${tags['display-name']}: ${message}`);
@@ -33,12 +35,13 @@ client.on('message', (channel, tags, message, self) => {
 client.on("cheer", (channel, userstate, message) => {
 
     if (userstate.bits >= 10 && userstate.bits != 5000) {
+        tl.restart();
         tl.fromTo(".smile", { y: -200 }, { duration: 2, delay: 1, ease: "bounce.out", y: 1180 });
         tl.to(".smile", { duration: 3, ease: " circ.in", x: 200 });
-        tl.restart();
     } else if (userstate.bits == 5000) {
-        sound.play();
-        tl.to(".bus", { duration: 15, repeat: 13, ease: " circ.in", x: -3000 });
         tl.restart();
+        sound.play();
+        tl.to(".bus", { duration: 15, repeat: 5, ease: " circ.in", x: -3000 });
+        sound.play();
     }
 });
