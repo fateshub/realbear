a74b0421e2b8349180b5b35890858e5bef80d02e
realbear <realbearinc@gmail.com>
2020-01-04 18:18:10 -0800

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -3,3 +3,21 @@ var tl = gsap.timeline({ repeat: Infinity, repeatDelay: 600 });
 //tl.fromTo(".smile", { x: -400, ease: " bounce.in" }, { duration: 1, x: 100 });
 tl.fromTo(".smile", { y: -200 }, { duration: 2, delay: 10, ease: "bounce.out", y: 980 });
 tl.to(".smile", { duration: 3, ease: " circ.in", x: 200 });
+
+
+
+
+const client = new tmi.Client({
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+    channels: ['scrubing']
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+    // "Alca: Hello, World!"
+    console.log(`${tags['display-name']}: ${message}`);
+});
