e7e461e110cc3d65556ab196568da0789a7daf8f
realbear <realbearinc@gmail.com>
2020-01-04 18:48:35 -0800

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,7 @@
-var tl = gsap.timeline({ repeat: Infinity, repeatDelay: 600 });
+var tl = gsap.timeline();
 
 //tl.fromTo(".smile", { x: -400, ease: " bounce.in" }, { duration: 1, x: 100 });
-tl.fromTo(".smile", { y: -200 }, { duration: 2, delay: 10, ease: "bounce.out", y: 980 });
-tl.to(".smile", { duration: 3, ease: " circ.in", x: 200 });
+
 
 
 
@@ -18,6 +17,21 @@ const client = new tmi.Client({
 client.connect();
 
 client.on('message', (channel, tags, message, self) => {
-    // "Alca: Hello, World!"
+    if (message == 'bingSmile' && tags['display-name'] == 'DrFate') {
+        tl.fromTo(".smile", { y: -200 }, { duration: 2, delay: 10, ease: "bounce.out", y: 1180 });
+        tl.to(".smile", { duration: 3, ease: " circ.in", x: 200 });
+        tl.restart();
+    }
+
     console.log(`${tags['display-name']}: ${message}`);
+
+});
+
+client.on("cheer", (channel, userstate, message) => {
+
+    if (userstate.bits == 10) {
+        tl.fromTo(".smile", { y: -200 }, { duration: 2, delay: 10, ease: "bounce.out", y: 1180 });
+        tl.to(".smile", { duration: 3, ease: " circ.in", x: 200 });
+        tl.restart();
+    }
 });
