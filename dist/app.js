26fea43c621bcb6544c46edc86d8fd3c32d599ea
realbear <realbearinc@gmail.com>
2020-01-20 21:38:31 -0800

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -19,6 +19,14 @@ function finger() {
     return;
 }
 
+function xx() {
+    tl.fromTo(".xx", { opacity: 0 }, { duration: 2, opacity: 1 }, );
+    tl.fromTo(".xx", { opacity: 1 }, { delay: 3, opacity: 0, duration: 2 });
+    tl.restart();
+    tl.remove();
+    return;
+}
+
 
 client.on('message', (channel, tags, message, self) => {
     console.log(`${tags['display-name']}: ${message}`);
@@ -28,6 +36,9 @@ client.on('message', (channel, tags, message, self) => {
     } else if (message == "!stop" && tags['display-name'] == "DrFate") {
         tl.remove();
         tl.restart();
+        return;
+    } else if (message == "TreatStreamTime" && tags['display-name'] == "DrFate") {
+        xx();
         return;
     }
 
