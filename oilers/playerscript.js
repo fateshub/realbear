7f37a7ef61eb24e7b99bbca3ac81bc4d69cc8d80
realbear <realbearinc@gmail.com>
2020-03-25 17:25:57 -0700

    Add files via upload

---
+++ oilers/playerscript.js
@@ -0,0 +1,17 @@
+
+
+const client = new tmi.Client({
+	connection: {
+		secure: true,
+		reconnect: true
+	},
+	channels: ['drfate']
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// "Alca: Hello, World!"
+    document.getElementById("player").src = message +"?autoplay=1";
+});
+		
