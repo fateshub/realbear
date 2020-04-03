488585df4a4ed41dd17771386de25f04fd495872
realbear <realbearinc@gmail.com>
2020-04-02 21:39:10 -0700

    Add files via upload

---
+++ emote/playerscript.js
@@ -0,0 +1,22 @@
+
+
+ComfyJS.Init( "scrubing" );
+ComfyJS.onChat = ( user, message, flags, self, extra ) => {
+	console.log( user, message );
+	console.log( extra.messageEmotes );
+	var emote = JSON.stringify(extra.messageEmotes).split(":");
+	if(flags.highlighted){
+	if(extra.messageEmotes){
+			document.getElementById("emotes").src = "https://static-cdn.jtvnw.net/emoticons/v1/"+emote[0].replace(/[^0-9 ]/g, "")+"/3.0";
+			var emoteanim = document.getElementById("emotes");
+			emoteanim.classList.add('animated','rollIn');
+			emoteanim.addEventListener("animationend", function(){
+				emoteanim.classList.remove('animated','rollIn')
+			});
+			
+
+		} 
+	}
+  }
+
+
