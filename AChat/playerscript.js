645989954ba40efb5e291838a1efe54f50578b39
realbear <realbearinc@gmail.com>
2020-04-04 21:23:41 -0700

    Add files via upload

--- AChat/playerscript.js
+++ AChat/playerscript.js
@@ -1,22 +1,30 @@
 
+//lmfao
+var timer = new Timer("100 milliseconds");
+
 
 ComfyJS.Init( "scrubing" );
 ComfyJS.onChat = ( user, message, flags, self, extra ) => {
 	console.log( user, message );
-		if(flags.highlighted){
+	if(flags.highlighted){
+	document.getElementById("container").style.visibility = "visible"; 
+	timer.reset()
 	document.getElementById("text").innerText = message;
 	document.getElementById("user").innerText = user;
-
 	console.log( extra.messageEmotes );
 	animation("buble","jackInTheBox");
 	animation("text","jackInTheBox");
 	animation("user","jackInTheBox");
     Actext()
-	window.fitText( document.getElementById("text"), 1.8 );
-	window.fitText( document.getElementById("user"), 0.8 );
-		}
+	window.fitText( document.getElementById("text"), 1.9 );
+	window.fitText( document.getElementById("user"), 0.9 );	
+	timer.bind('15 seconds', function () {	
+		document.getElementById("container").style.visibility = "hidden"; 
+		console.log('timer event every 1 minute, 20 seconds');
+ 	});
+	 timer.start();
   }
-
+}
   function animation(parameter1, parameter2) {
 	var buble = document.getElementById(parameter1);
 	buble.classList.add('animated',parameter2);
@@ -24,7 +32,6 @@ ComfyJS.onChat = ( user, message, flags, self, extra ) => {
 	buble.classList.remove('animated',parameter2)
 	});
   } 
-
   function dataURItoBlob(dataURI) {
 	// convert base64/URLEncoded data component to raw binary data held in a string
 	var byteString;
