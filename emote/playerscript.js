4b182c11d0a825a553e2bc3f863569ef9eb4afbd
realbear <realbearinc@gmail.com>
2020-04-04 19:35:13 -0700

    Update playerscript.js

--- emote/playerscript.js
+++ emote/playerscript.js
@@ -6,7 +6,7 @@ ComfyJS.onChat = ( user, message, flags, self, extra ) => {
 	console.log( extra.messageEmotes );
 	var emote = JSON.stringify(extra.messageEmotes).split(":");
 	if(flags.highlighted){
-	if(extra.messageEmotes){
+	if(extra.messageEmotes && user != "Vore"){
 			document.getElementById("emotes").src = "https://static-cdn.jtvnw.net/emoticons/v1/"+emote[0].replace(/[^0-9 ]/g, "")+"/3.0";
 			var emoteanim = document.getElementById("emotes");
 			emoteanim.classList.add('animated','tada');
