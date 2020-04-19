8b6e34b746d016d7e6eff82ab84546ad849a6fc7
realbear <realbearinc@gmail.com>
2020-04-19 12:19:36 -0700

    Update main.js

--- bingc/main.js
+++ bingc/main.js
@@ -39,6 +39,23 @@ var app = new Vue({
 
   }
 
+    
+   
+  ComfyJS.onChat = ( user, message, flags, self, extra  ) => {
+    console.log( user, message );
+    if(user.toLowerCase() == "drfate" && message === "testing"){
+    tl.to('h1',{ duration: 3,  ease: "bounce. in" ,y: 300}).call(playSound);
+    tl.to(wave,{ duration: 1, y: -200,  ease: "bounce. out"});
+  tl.to(waveb,{ duration: 1, y: -200,  ease: "bounce. out"});
+  tl.to(bing,{ duration: 16, x: -1808,ease: " expo. out"}, ">");
+  tl.to(bing,{ duration: 0, x: 1408}, ">");
+  tl.to(wave,{ duration: 1, y: 400});
+  tl.to(waveb,{ duration: 1, y: 400});
+  tl.to('h1',{ duration: 2, y: -300});
+
+  }
+} 
+    
 ComfyJS.Init( "scrubing" );
 
 
