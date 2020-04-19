c808ff21f717970bbb1f4006b3394c4673dc70d3
realbear <realbearinc@gmail.com>
2020-04-19 11:15:01 -0700

    Add files via upload

---
+++ bingc/main.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Welcome To The Furry Pirates '
+    }
+  })
+
+  var bing = document.getElementById("bingc");
+  var wave = document.getElementById("wave");
+  var waveb = document.getElementById("waveb");
+  var audio = new Audio('sound.mp3');
+  var tl = gsap.timeline();
+
+
+
+  ComfyJS.onSub = ( user, message, subTierInfo, extra  ) => {
+    console.log( user, message );
+    tl.to('h1',{ duration: 3,  ease: "bounce. in" ,y: 300}).call(playSound);
+    tl.to(wave,{ duration: 1, y: -200,  ease: "bounce. out"});
+  tl.to(waveb,{ duration: 1, y: -200,  ease: "bounce. out"});
+  tl.to(bing,{ duration: 16, x: -1808,ease: " expo. out"}, ">");
+  tl.to(bing,{ duration: 0, x: 1408}, ">");
+  tl.to(wave,{ duration: 1, y: 400});
+  tl.to(waveb,{ duration: 1, y: 400});
+  tl.to('h1',{ duration: 2, y: -300});
+
+  }
+
+ComfyJS.Init( "drfate" );
+
+
+function playSound(){
+  audio.play();  
+}
